148e5a4fc7a8174693f467bc48957e2b67f03d7e
MarzenkaS <>
2023-10-18 16:38:12 +0000

    Add if/elif/else statement to selectig_a_season function

--- run.py
+++ run.py
@@ -5,9 +5,16 @@ def selecting_a_season():
     """
     Collecting data from users regarding preferred season for holiday
     """
+    cold_choice = "winter"
+    warm_choice = "summer"
     print('Are you interested in a holiday in the summer or winter season?\n')
     user_answer = input('Please enter your answer here by using one word:')
-    print(user_answer)
-    
+    if user_answer == cold_choice:
+        print(user_answer)
+    elif user_answer == warm_choice:
+        print(user_answer)
+    else:
+        print('Please try again and enter correct word')
+
 
 selecting_a_season()
