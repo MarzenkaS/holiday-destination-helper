e7725e9783c5f67d78ae9f5d6ece06219723035a
MarzenkaS <>
2023-10-18 17:41:36 +0000

    Add summer_temp_choice which collects user choice for temperature

--- run.py
+++ run.py
@@ -5,16 +5,36 @@ def selecting_a_season():
     """
     Collecting data from users regarding preferred season for holiday
     """
-    cold_choice = "winter"
     warm_choice = "summer"
+    cold_choice = "winter"
     print('Are you interested in a holiday in the summer or winter season?\n')
     user_answer = input('Please enter your answer here by using one word:')
-    if user_answer == cold_choice:
-        print(user_answer)
-    elif user_answer == warm_choice:
-        print(user_answer)
+    if user_answer == warm_choice:  
+        print()
+    elif user_answer == cold_choice:
+        print()  
     else:
         print('Please try again and enter correct word')
 
 
 selecting_a_season()
+
+
+def summer_temp_choice():
+    """
+    Collecting user choice for temperature
+    """
+    hot = "below"
+    hotter = "above"
+    print('What weather are you interested in? Below or above 30 degrees\n')
+    user_answer = input('Please enter your answer here by using one word:')
+    if user_answer == hot:  
+        print()
+    elif user_answer == hotter:
+        print()  
+    else:
+        print('Please try again and enter correct word')
+
+
+summer_temp_choice()    
+    
