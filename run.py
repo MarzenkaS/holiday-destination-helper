7901f60b0ec0fadb5f9186bf2dc7bd4512069d65
MarzenkaS <>
2023-10-18 15:39:34 +0000

    Add selecting_a_season function where user can enter a selectionof their preferred season

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import math
+
+
+def selecting_a_season():
+    """
+    Collecting data from users regarding preferred season for holiday
+    """
+    print('Are you interested in a holiday in the summer or winter season?\n')
+    user_answer = input('Please enter your answer here by using one word:')
+    print(user_answer)
+    
+
+selecting_a_season()
