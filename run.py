e54be539a37984ff6ba134ab29a917dfbbd7545a
MarzenkaS <>
2023-10-23 15:40:08 +0000

    Create function temp_above_activities for collecting data from user and giving theme answer about preferred holida destination

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def selecting_a_season():
     elif user_answer == cold_choice:
         print()
     else:
-        print('Please try again and enter correct word')
+        print('Please try again and enter correct word!')
 
 
 selecting_a_season()
@@ -27,42 +27,68 @@ def summer_temp_choice():
     """
     hot = "below"
     hotter = "above"
-    print('What weather are you interested in? Below or above 30 degrees\n')
+    print('What weather are you interested in? Below or above 30 degrees?\n')
     user_answer = input('Please enter your answer here by using one word:')
     if user_answer == hot:
         print()
     elif user_answer == hotter:
         print()
     else:
-        print('Please try again and enter correct word')
+        print('Please try again and enter correct word!')
 
 
 summer_temp_choice()
 
 
-def below_activities():
+def temp_below_activities():
     """
     Collecting data from user about preferred activities.
+    Suggestions of holiday destinations selected based on
+    data entered by the user.
     """
-    first = "visiting_city"
-    second = "water_activities"
+    first = "visiting city"
+    second = "water activities"
     third = "hiking"
-    print('Wich activities interest you the most? First: visiting city,\n second: water activities, third: hiking.\n')
+    print('Which activities interest you the most?')
+    print('First: visiting city, second: water activities, third: hiking.')
     user_answer = input('Please enter your answer here:')
     if user_answer == first:
-        print()
+        print('The best would be to visit Poland, Germany,' +
+              'Austria and Czech Republic.')
     elif user_answer == second:
-        print()
+        print('Good idea is to travel to Poland, Germany or France.')
     elif user_answer == third:
-        print()
+        print('The best places for hiking you will find in Austria,' +
+              'Switzerland, Slovakia, Germany, Poland.\n')
     else:
-        print('Please try again and enter correct word')
+        print('Please try again and enter correct word!')
 
 
-below_activities()
+temp_below_activities()
 
 
-def suggested_places():
+def temp_above_activities():
     """
-    Presenting holiday destinations to the user.
+    Collecting data from user about preferred activities.
+    Suggestions of holiday destinations selected based on
+    data entered by the user.
     """
+    first = "visiting city"
+    second = "water activities"
+    third = "hiking"
+    print('Which activities interest you the most?')
+    print('First: visiting city, second: water activities, third: hiking.')
+    user_answer = input('Please enter your answer here:')
+    if user_answer == first:
+        print('The best would be to visit Spain, Italy,' +
+              'Greece or France')
+    elif user_answer == second:
+        print('How about Cyprus, Spain, Italy, Greece, Maldives or Zanzibar')
+    elif user_answer == third:
+        print('The best places for hiking you will find in' +
+              'Italy, Spain or France')
+    else:
+        print('Please try again and enter correct word!')
+            
+
+temp_above_activities()
